73448c79b8e5c82a2ca67949893a68d5b4019827
everardo <eve.bmdz@gmail.com>
2022-07-14 16:11:04 -0500

    rodenar por fecha divulgacion

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -24,7 +24,7 @@ class WelcomeController extends Controller
     {
         $eventos = Evento::where('activo','=',1)->orderBy('fecha','DESC')->take(3)->get();
         $investigadores = Investigador::where('activo','=',1)->orderBy('apellido','asc')->take(3)->get();
-        $divulgaciones = Publicacion::where('activo','=',1)->where('categoria','=',3)->orderBy('titulo','desc')->take(3)->get();
+        $divulgaciones = Publicacion::where('activo','=',1)->where('categoria','=',3)->orderBy('fecha','desc')->take(3)->get();
         $secciones = QuienesSomos::where('activo','=',1)->get();
         $contactos = Contacto::where('activo','=',1)->get();
         return view('welcome',['eventos'=>$eventos,'investigadores'=>$investigadores,'divulgaciones'=>$divulgaciones, 'secciones'=>$secciones, 'contactos'=>$contactos]);
