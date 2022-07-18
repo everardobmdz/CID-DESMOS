62c7c08e8ea69d3941245afea63516fa53c67d76
everardo <eve.bmdz@gmail.com>
2022-07-18 10:08:09 -0500

    divulgaciones

--- resources/views/divulgaciones/show.blade.php
+++ resources/views/divulgaciones/show.blade.php
@@ -30,7 +30,7 @@
             <iframe 
                 width="560" 
                 height="315" 
-                src="{{'https://www.youtube.com/embed/'.explode('=',$divulgacion->link)[1]}}"
+                src="{{'https://www.youtube.com/embed/'.explode('youtu.be/',$divulgacion->link)[1]}}"
                 title="YouTube video player" 
                 frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
